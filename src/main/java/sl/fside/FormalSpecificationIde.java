06cae53ac67ac402ae0278719ed4fc414adabffb
Szymon Lepianka <slepianka@wp.pl>
2023-03-15 22:09:29 +0100

    reduces height of the main screen

--- src/main/java/sl/fside/FormalSpecificationIde.java
+++ src/main/java/sl/fside/FormalSpecificationIde.java
@@ -1,33 +1,36 @@
 package sl.fside;
 
-import javafx.stage.Stage;
-import sl.fside.services.*;
 import com.google.inject.*;
 import javafx.application.*;
 import javafx.fxml.*;
 import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.stage.*;
+import javafx.stage.Stage;
+import sl.fside.services.*;
 
 public class FormalSpecificationIde extends Application {
 
     private final Injector injector = Guice.createInjector(new MainModule());
 
     private final IResourceService resourceService = injector.getInstance(IResourceService.class);
-    private FXMLLoader fxmlLoader;
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle(resourceService.getText("ApplicationTitle"));
-        stage.setResizable(false);
 
-        fxmlLoader = new FXMLLoader(getClass().getResource("ui/MainWindow.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/MainWindow.fxml"));
         fxmlLoader.setControllerFactory(injector::getInstance);
         var root = new AnchorPane();
         fxmlLoader.setRoot(root);
         fxmlLoader.<AnchorPane>load();
-        stage.setScene(new Scene(root));
 
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(root);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+
+        stage.setScene(new Scene(scrollPane));
         stage.show();
     }
 }
