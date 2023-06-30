53dfc25a80c4420f2ea1e2f9342cfa7a1d720f80
Szymon Lepianka <slepianka@wp.pl>
2023-06-30 22:12:04 +0200

    adds version 3 of kompilotory_projekt_labuz_kubala_borowy to FSIDE

--- src/main/java/sl/fside/services/code_generator2/App.java
+++ src/main/java/sl/fside/services/code_generator2/App.java
@@ -7,9 +7,10 @@ public class App {
 //        String pattern = "SeqSeq(eat(\"amount\": \"int\"),Cond(tried(),watchTheWorld(\"amount\": \"int\"),composeLetters(\"number\": \"int\"),beHappy(\"amount\": \"int\")),sleep(\"hours\"))";
 //        String pattern = "SeqSeq(eat,Cond(tried,watchTheWorld(),composeLetters(),beHappy()),sleep())";
 //        String pattern = "Seq(Seq(\"a\",\"b\"),\"c\")";
-        String pattern = "Seq(Seq(aaaa,baaaa),caaaaaaaaaaaaaasadfaxfxsdf)";
+//        String pattern = "Seq(Branch(a,b,c), BranchRe(d,e,f))";
+        String pattern = "Seq(Concur(a,b,c), ConcurRe(d,e,f))";
 //            String pattern = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
-        String code = Compiler.compile(pattern, Compiler.Language.PYTHON);
+        String code = Compiler.compile(pattern, Compiler.Language.JAVA);
         System.out.print(code);
     }
 }
